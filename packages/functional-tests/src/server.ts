422366be42f0555491b4d46581e638c5b74dc9f6
Jacob Bashista <tgb.bashista@gmail.com>
2022-08-20 11:03:40 -0400

    Demo add new requests

--- packages/functional-tests/src/server.ts
+++ packages/functional-tests/src/server.ts
@@ -3,16 +3,23 @@
  * Licensed under the MIT License.
  */
 
-import * as MRE from '@microsoft/mixed-reality-extension-sdk';
-import dotenv from 'dotenv';
-import { resolve as resolvePath } from 'path';
-import { App } from './app';
+import * as MRE from "@microsoft/mixed-reality-extension-sdk";
+import dotenv from "dotenv";
+import { resolve as resolvePath } from "path";
+import { App } from "./app";
+
+enum RequestType {
+	GET,
+	POST,
+}
 
 // Read .env if file exists
 dotenv.config();
 
-process.on('uncaughtException', (err) => console.log('uncaughtException', err));
-process.on('unhandledRejection', (reason) => console.log('unhandledRejection', reason));
+process.on("uncaughtException", (err) => console.log("uncaughtException", err));
+process.on("unhandledRejection", (reason) =>
+	console.log("unhandledRejection", reason)
+);
 
 // MRE.log.enable('network');
 // MRE.log.enable('network-content');
@@ -20,14 +27,24 @@ process.on('unhandledRejection', (reason) => console.log('unhandledRejection', r
 // Start listening for connections, and serve static files
 const server = new MRE.WebHost({
 	// baseUrl: 'http://<ngrok-id>.ngrok.io',
-	baseDir: resolvePath(__dirname, '../public')
+	baseDir: resolvePath(__dirname, "../public"),
+	endpoints: [
+		{
+			type: RequestType.GET,
+			url: "/api/hello",
+			handler(req, res, next) {
+				res.send(200, { hi: "hello" });
+				next();
+			},
+		},
+	],
 });
 
 // Handle new application sessions
 server.adapter.onConnection((context, params) => new App(context, params));
 
 server.ready.then(() => {
-	server.adapter.server.get('/test/timeout/:file', (req, res) => {
+	server.adapter.server.get("/test/timeout/:file", (req, res) => {
 		console.log("Will time out eventually:", req.url);
 		// no res.send
 	});
--- packages/sdk/src/util/webHost.ts
+++ packages/sdk/src/util/webHost.ts
@@ -3,24 +3,39 @@
  * Licensed under the MIT License.
  */
 
-import * as Restify from 'restify';
-import { NotFoundError } from 'restify-errors';
-import etag from 'etag';
+import * as Restify from "restify";
+import { NotFoundError } from "restify-errors";
+import etag from "etag";
 
-import { validate } from 'jsonschema';
-import manifestSchema from './manifestSchema.json';
-import { resolve } from 'path';
-import { readFile as _readFile } from 'fs';
-import { promisify } from 'util';
+import { validate } from "jsonschema";
+import manifestSchema from "./manifestSchema.json";
+import { resolve } from "path";
+import { readFile as _readFile } from "fs";
+import { promisify } from "util";
 const readFile = promisify(_readFile);
 
-import { log, MultipeerAdapter, Permissions } from '..';
-import { Adapter } from '../internal';
+import { log, MultipeerAdapter, Permissions } from "..";
+import { Adapter } from "../internal";
 
 type StaticBufferInfo = {
 	buffer: Buffer;
 	etag: string;
 	contentType: string;
+};
+
+enum RequestType {
+	GET,
+	POST,
+}
+
+type WebRequest = {
+	type: RequestType;
+	url: string;
+	handler: (
+		req: Restify.Request,
+		res: Restify.Response,
+		next: Restify.Next
+	) => void;
 };
 
 /**
@@ -32,27 +47,36 @@ export class WebHost {
 	private manifest: Buffer = null;
 	private _ready: Promise<void>;
 
-	public get adapter() { return this._adapter; }
-	public get baseDir() { return this._baseDir; }
+	public get adapter() {
+		return this._adapter;
+	}
+	public get baseDir() {
+		return this._baseDir;
+	}
 
 	/**
 	 * A promise that resolves when the HTTP server is listening.
 	 * Get the server reference from `webHost.adapter.server`.
 	 */
-	public get ready() { return this._ready; }
+	public get ready() {
+		return this._ready;
+	}
 
 	private bufferMap: { [path: string]: StaticBufferInfo } = {};
 
-	public constructor(options: {
-		baseDir?: string;
-		baseUrl?: string;
-		port?: string | number;
-		permissions?: Permissions[];
-		optionalPermissions?: Permissions[];
-	} = {}) {
-		const pjson = require('../../package.json'); /* eslint-disable-line @typescript-eslint/no-var-requires */
-		log.info('app', `Node: ${process.version}`);
-		log.info('app', `${pjson.name}: v${pjson.version}`);
+	public constructor(
+		options: {
+			baseDir?: string;
+			baseUrl?: string;
+			port?: string | number;
+			permissions?: Permissions[];
+			optionalPermissions?: Permissions[];
+			endpoints?: WebRequest[];
+		} = {}
+	) {
+		const pjson = require("../../package.json"); /* eslint-disable-line @typescript-eslint/no-var-requires */
+		log.info("app", `Node: ${process.version}`);
+		log.info("app", `${pjson.name}: v${pjson.version}`);
 
 		this._baseDir = options.baseDir || process.env.BASE_DIR;
 
@@ -63,52 +87,98 @@ export class WebHost {
 		this._adapter = new MultipeerAdapter({ port });
 
 		// Start listening for new app connections from a multi-peer client.
-		const serverP = this.validateManifest().then(() => this._adapter.listen());
+		const serverP = this.validateManifest().then(() =>
+			this._adapter.listen()
+		);
 		this._ready = serverP.then<void>();
-		serverP.then(server => {
-			log.info('app', `${server.name} listening on ${JSON.stringify(server.address())}`);
-			log.info('app', `baseDir: ${this.baseDir}`);
+		serverP
+			.then((server) => {
+				log.info(
+					"app",
+					`${server.name} listening on ${JSON.stringify(
+						server.address()
+					)}`
+				);
+				log.info("app", `baseDir: ${this.baseDir}`);
 
-			// check if a procedural manifest is needed, and serve if so
-			this.serveManifestIfNeeded(server, options.permissions, options.optionalPermissions);
+				// check if a procedural manifest is needed, and serve if so
+				this.serveManifestIfNeeded(
+					server,
+					options.permissions,
+					options.optionalPermissions
+				);
 
-			// serve static buffers
-			server.get(`/buffers/:name`,
-				this.checkStaticBuffers,
-				Restify.plugins.conditionalRequest(),
-				this.serveStaticBuffers);
+				// Create custom endpoints
+				if (options.endpoints) {
+					options.endpoints.forEach((endpoint) => {
+						if (endpoint.type === RequestType.GET) {
+							server.get(endpoint.url, endpoint.handler);
+						} else {
+							server.post(endpoint.url, endpoint.handler);
+						}
+					});
+				}
 
-			// serve static files
-			if (this.baseDir) {
-				server.get('/*', Restify.plugins.serveStaticFiles(this._baseDir));
-			}
-		})
-		.catch(reason => log.error('app', `Failed to start HTTP server: ${reason}`));
+				// serve static buffers
+				server.get(
+					`/buffers/:name`,
+					this.checkStaticBuffers,
+					Restify.plugins.conditionalRequest(),
+					this.serveStaticBuffers
+				);
+
+				// serve static files
+				if (this.baseDir) {
+					server.get(
+						"/*",
+						Restify.plugins.serveStaticFiles(this._baseDir)
+					);
+				}
+			})
+			.catch((reason) =>
+				log.error("app", `Failed to start HTTP server: ${reason}`)
+			);
 	}
 
-	private checkStaticBuffers = (req: Restify.Request, res: Restify.Response, next: Restify.Next) => {
+	private checkStaticBuffers = (
+		req: Restify.Request,
+		res: Restify.Response,
+		next: Restify.Next
+	) => {
 		const info = this.bufferMap[req.params.name];
 		if (info) {
-			res.setHeader('ETag', info.etag);
+			res.setHeader("ETag", info.etag);
 			next();
 		} else {
-			next(new NotFoundError(`No buffer registered under name ${req.params.name}`));
+			next(
+				new NotFoundError(
+					`No buffer registered under name ${req.params.name}`
+				)
+			);
 		}
 	};
 
-	private serveStaticBuffers = (req: Restify.Request, res: Restify.Response, next: Restify.Next) => {
+	private serveStaticBuffers = (
+		req: Restify.Request,
+		res: Restify.Response,
+		next: Restify.Next
+	) => {
 		const info = this.bufferMap[req.params.name];
 		if (info) {
 			res.contentType = info.contentType;
 			res.sendRaw(200, info.buffer);
 			next();
 		} else {
-			next(new NotFoundError(`No buffer registered under name ${req.params.name}`));
+			next(
+				new NotFoundError(
+					`No buffer registered under name ${req.params.name}`
+				)
+			);
 		}
 	};
 
 	private async validateManifest() {
-		const manifestPath = resolve(this.baseDir, './manifest.json');
+		const manifestPath = resolve(this.baseDir, "./manifest.json");
 
 		try {
 			this.manifest = await readFile(manifestPath);
@@ -118,35 +188,49 @@ export class WebHost {
 
 		let manifestJson: any;
 		try {
-			manifestJson = JSON.parse(this.manifest.toString('utf8'));
+			manifestJson = JSON.parse(this.manifest.toString("utf8"));
 		} catch {
-			log.error('app', `App manifest "${manifestPath}" is not JSON`);
+			log.error("app", `App manifest "${manifestPath}" is not JSON`);
 			this.manifest = null;
 			return;
 		}
 
 		const result = validate(manifestJson, manifestSchema);
 		if (!result.valid) {
-			log.error('app', `App manifest "${manifestPath}" is not valid:\n${result.errors.join('\n')}`);
+			log.error(
+				"app",
+				`App manifest "${manifestPath}" is not valid:\n${result.errors.join(
+					"\n"
+				)}`
+			);
 			this.manifest = null;
 			return;
 		}
 	}
 
 	private serveManifestIfNeeded(
-		server: Restify.Server, permissions?: Permissions[], optionalPermissions?: Permissions[]
+		server: Restify.Server,
+		permissions?: Permissions[],
+		optionalPermissions?: Permissions[]
 	) {
 		// print warning if no manifest supplied
 		if (!this.manifest && !permissions && !optionalPermissions) {
-			log.warning('app',
+			log.warning(
+				"app",
 				"No MRE manifest provided, and no permissions requested! For this MRE to use protected features, " +
-				`provide an MRE manifest at "${resolve(this.baseDir, './manifest.json')}", or pass a permissions ` +
-				"list into the WebHost constructor.");
+					`provide an MRE manifest at "${resolve(
+						this.baseDir,
+						"./manifest.json"
+					)}", or pass a permissions ` +
+					"list into the WebHost constructor."
+			);
 		}
 
-		server.get('/manifest.json', (_, res, next) => {
+		server.get("/manifest.json", (_, res, next) => {
 			if (this.manifest) {
-				res.sendRaw(200, this.manifest, { "Content-Type": "application/json" });
+				res.sendRaw(200, this.manifest, {
+					"Content-Type": "application/json",
+				});
 			} else if (permissions || optionalPermissions) {
 				res.json(200, { permissions, optionalPermissions });
 			} else {
@@ -163,11 +247,15 @@ export class WebHost {
 	 * @param contentType The MIME type that identifies this blob
 	 * @returns The URL to fetch the provided blob
 	 */
-	public registerStaticBuffer(filename: string, blob: Buffer, contentType = 'application/octet-stream'): string {
+	public registerStaticBuffer(
+		filename: string,
+		blob: Buffer,
+		contentType = "application/octet-stream"
+	): string {
 		this.bufferMap[filename] = {
 			buffer: blob,
 			etag: etag(blob),
-			contentType
+			contentType,
 		};
 		return `buffers/${filename}`;
 	}
